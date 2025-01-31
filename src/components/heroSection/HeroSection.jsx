1d1d706727f2d794160135fd3108d892da0e7837
Juan Esparza <83143208+JEWebDev@users.noreply.github.com>
2025-01-31 02:40:28 -0600

    Finish responsive styles for Hero Section
    Finished styles for tablet and desktop view along
    with minor visual fixes on another components

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -8,22 +8,24 @@ export function HeroSection({ heading, description }) {
   return (
     <section className="hero-section">
       <Header></Header>
-      <div className="hero__text">
-        <h1 className="gradient">{heading}</h1>
-        <p>{description}</p>
-        <div className="hero-cta">
-          <MainButton
-            text={"review membership options"}
-            img={"assets/images/icon-arrow-down.svg"}
-          ></MainButton>
-          <Joined number={200}></Joined>
+      <div className="content">
+        <div className="hero__text">
+          <h1 className="gradient">{heading}</h1>
+          <p className="hero__description">{description}</p>
+          <div className="hero-cta">
+            <MainButton
+              text={"review membership options"}
+              img={"assets/images/icon-arrow-down.svg"}
+            ></MainButton>
+            <Joined number={200}></Joined>
+          </div>
         </div>
-      </div>
 
-      <picture>
-        <img src="assets/images/image-hero-mobile.webp" alt="" />
-      </picture>
-      <div className="glow"></div>
+        <picture>
+          <img src="assets/images/image-hero-mobile.webp" alt="" />
+        </picture>
+        <div className="glow"></div>
+      </div>
     </section>
   )
 }
--- src/components/joined/Joined.jsx
+++ src/components/joined/Joined.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
 import "./joined.css"
 import { Rating } from "/src/components/rating/Rating"
-export function Joined({ number }) {
+export function Joined() {
   return (
     <div className="card">
       <img
         className="avatar"
         src="assets/images/image-avatars.webp"
         alt="People who already joined"
+        width="110px"
+        height="40px"
       />
       <div className="joined">
         <Rating></Rating>
-        <p>
-          <span>{number}</span>+ developers joined already
-        </p>
+        <p>200+ developers joined already</p>
       </div>
     </div>
   )
